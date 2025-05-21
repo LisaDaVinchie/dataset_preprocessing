d99a6df3c98cb689002489d845c3a51e34674d48
LisaDaVinchie <lisadavinchie@gmail.com>
2025-05-21 12:20:36 +0200

    Refactor dataset handling in download script to improve clarity and support biochemistry subfolder downloads

--- src/download.py
+++ src/download.py
@@ -11,21 +11,32 @@ def main():
     params, paths = parse_input_paths()
     print("Input paths parsed successfully\n")
 
-    dataset_name = params["dataset"]["dataset_name"]
+    dataset_name = params["dataset"]["dataset_kind"]
         
     raw_data_dir = Path(paths[dataset_name]["raw_data_dir"])
     if not raw_data_dir.exists():
         raise FileNotFoundError(f"Directory {raw_data_dir} does not exist.")
 
-    longitude_range = list(params[dataset_name]["longitude_range"])
-    latitude_range = list(params[dataset_name]["latitude_range"])
-    depth_range = list(params[dataset_name]["depth_range"])
-    channels = list(params[dataset_name]["channels"])
-    dataset_id = str(params[dataset_name]["dataset_id"])
     year_range = list(params["dataset"]["year_range"])
     month_range = list(params["dataset"]["month_range"])
-
     datetime_range = get_datetime_range(year_range, month_range)
+    
+    dataset_params = params["dataset"][dataset_name]
+    longitude_range = list(dataset_params["longitude_range"])
+    latitude_range = list(dataset_params["latitude_range"])
+    depth_range = list(dataset_params["depth_range"])
+    dataset_id = str(dataset_params["dataset_id"])
+    
+    if dataset_name == "biochemistry":
+        subfolders = list(dataset_params["subfolders"])
+        dataset_base_id = str(dataset_params["dataset_id"])
+        channels = {}
+        dataset_id = {}
+        for subfolder in subfolders:
+            channels[subfolder] = list(dataset_params[subfolder]["channels"])
+            dataset_id[subfolder] = dataset_base_id.replace("name", subfolder)
+    else:
+        channels = list(dataset_params["channels_to_keep"])
 
     login_copernicus()
     print("\nLogin completed\n")
@@ -36,19 +47,33 @@ def main():
         depth_range=depth_range
     )
     print("\nCopernicus Marine Downloader initialized\n")
-    # Download the data
-
-    for year in range(year_range[0], year_range[1] + 1):
-        for month in range(month_range[0], month_range[1] + 1):
-            print(f"\nDownloading data for {year}-{month}...\n")
-            dwl.download(
-                output_filename=f"{year}_{str(month).zfill(2)}.nc",
-                dataset_id=dataset_id,
-                output_directory=raw_data_dir,
-                variables=channels,
-                datetime_range=datetime_range[(year, month)]
-            )
-            print(f"\nDownload completed for {year}-{month} in {time.time() - start_time} seconds\n")
+    if dataset_name == "biochemistry":
+        for year in range(year_range[0], year_range[1] + 1):
+            for month in range(month_range[0], month_range[1] + 1):
+                print(f"\nDownloading data for {year}-{month}...\n")
+                for subfolder in subfolders:
+                    print(f"\nDownloading {subfolder}\n")
+                    dwl.download(
+                        output_filename=f"{year}_{str(month).zfill(2)}.nc",
+                        dataset_id=dataset_id[subfolder],
+                        output_directory=raw_data_dir,
+                        variables=channels[subfolder],
+                        datetime_range=datetime_range[(year, month)]
+                    )
+                    print(f"\nDownload completed for {year}-{month} in {time.time() - start_time} seconds\n")
+    else:
+        # Download the data
+        for year in range(year_range[0], year_range[1] + 1):
+            for month in range(month_range[0], month_range[1] + 1):
+                print(f"\nDownloading data for {year}-{month}...\n")
+                dwl.download(
+                    output_filename=f"{year}_{str(month).zfill(2)}.nc",
+                    dataset_id=dataset_id,
+                    output_directory=raw_data_dir,
+                    variables=channels,
+                    datetime_range=datetime_range[(year, month)]
+                )
+                print(f"\nDownload completed for {year}-{month} in {time.time() - start_time} seconds\n")
 
     print(f"\nDownload completed in {time.time() - start_time} seconds\n")
 
